9c5db4af0276d941fc37edb16d952ddefdb56fa4
CHINMAY BHAT <chinmaybhat29@gmail.com>
2023-05-12 21:31:46 +0530

    Copied model classes to Abstraction,Business and  Repository projects

---
+++ DRS.ExpenseManagementSystem.UI/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DRS.ExpenseManagementSystem.Abstraction.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? EmployeeCode { get; set; }
+
+    public string? Password { get; set; }
+
+    public bool? IsAccountLocked { get; set; }
+
+    public bool? IsActive { get; set; }
+
+    public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+}
