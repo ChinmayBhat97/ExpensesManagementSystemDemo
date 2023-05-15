d86ffcb481b52166f4a7f28df2efc60b0ed64f22
Anish Ani <anishani0078@gmail.com>
2023-05-15 17:20:27 +0530

    employee repo created

--- DRS.ExpenseManagementSystem.Abstraction/Models/Department.cs
+++ DRS.ExpenseManagementSystem.Abstraction/Models/Department.cs
@@ -12,7 +12,7 @@ public partial class Department
         }
 
         public int DeptId { get; set; }
-        public string? DepartementName { get; set; }
+        public string DepartementName { get; set; }
 
         public virtual ICollection<Employee> Employees { get; set; }
         public virtual ICollection<ExpensesClaim> ExpensesClaims { get; set; }
--- DRS.ExpenseManagementSystem.Repository/Repository/EployeeRepository.cs
+++ DRS.ExpenseManagementSystem.Repository/Repository/EployeeRepository.cs
@@ -1,18 +1,36 @@
-﻿using DRS.ExpenseManagementSystem.Abstraction.Repository;
+﻿using DRS.ExpenseManagementSystem.Abstraction.Models;
+using DRS.ExpenseManagementSystem.Abstraction.Repository;
+using DRS.ExpenseManagementSystem.Repository.DatabaseContext;
+using DRS.ExpenseManagementSystem.Repository.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DRS.ExpenseManagementSystem.Repository.Repository
+namespace DRS.ExpenseManagementSystem.Repository
 {
-    public class EmployeeRepository : BaseRepository<Tenant>, ITenantRepository
+    public class EmployeeRepository : BaseRepository<Employee>, IEmployeeRepository
     {
-        private ScaffoldDbContext databaseContext;
-        public TenantRepository(ScaffoldDbContext databaseContext) : base(databaseContext)
+        private ExpenseManagementSystemContext databaseContext;
+        public EmployeeRepository(ExpenseManagementSystemContext databaseContext) : base(databaseContext)
         {
             this.databaseContext = databaseContext;
+        }
+
+        public Task<List<Employee>> GetByDept(string dept)
+        {
+            return databaseContext.Employees.AsQueryable().Where(x => x.Dept = dept).ToList();
+        }
+
+        public Task<List<Employee>> GetByEmpFirstName(string empFirstName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetByEmpId(int empid)
+        {
+            throw new NotImplementedException();
         }
     }
 }
