23ac1fc1e507b65e415bee8b071c368f262b5a57
Anish Ani <anishani0078@gmail.com>
2023-05-15 11:17:48 +0530

    added a eployee repo

---
+++ DRS.ExpenseManagementSystem.Abstraction/Repository/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using DRS.ExpenseManagementSystem.Abstraction.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRS.ExpenseManagementSystem.Abstraction.Repository
+{
+    public interface IEmployeeRepository: IBaseRepository<Employee>
+    {
+    }
+}
