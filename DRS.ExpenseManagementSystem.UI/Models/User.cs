f22fb939eee094e22ffa4b8fae939472f96edf71
Anish Ani <anishani0078@gmail.com>
2023-05-16 14:07:29 +0530

    added login model a

---
+++ DRS.ExpenseManagementSystem.UI/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace DRS.ExpenseManagementSystem.Abstraction.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Employees = new HashSet<Employee>();
+        }
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Username field is required")]
+        [Display(Name = "Username")]
+        public string? EmployeeCode { get; set; }
+
+        [Required(ErrorMessage = "Password field is required")]
+        [Display(Name = "Password")]
+        public string? Password { get; set; }
+        public bool? IsAccountLocked { get; set; }
+        public bool? IsActive { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
