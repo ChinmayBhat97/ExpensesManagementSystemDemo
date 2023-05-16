a290eb8540937290bef5c6d8f25db22b2c37ff3d
Anish Ani <anishani0078@gmail.com>
2023-05-17 00:08:22 +0530

    completed the user viewmodel and controller

--- DRS.ExpenseManagementSystem.UI/Controllers/UserController.cs
+++ DRS.ExpenseManagementSystem.UI/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using DRS.ExpenseManagementSystem.UI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
+using DRS.ExpenseManagementSystem.Abstraction.ViewModels;
+using UserViewModel = DRS.ExpenseManagementSystem.UI.Models.UserViewModel;
 
 namespace DRS.ExpenseManagementSystem.UI.Controllers
 {
@@ -20,6 +22,63 @@ namespace DRS.ExpenseManagementSystem.UI.Controllers
                 BaseAddress = new Uri(configuration["BaseUrl"]),
                 Timeout = TimeSpan.FromMinutes(5)
             };
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage responseUser = await client.GetAsync(client.BaseAddress + $"User");
+            return View();
+        }
+
+        [HttpGet("User/Create")]
+        public async Task<IActionResult> Create()
+        {
+            HttpResponseMessage responseCreateUser = await client.GetAsync(client.BaseAddress + $"User");
+            return View();
+
+        }
+
+
+        [HttpPost("User/Create")]
+        public async Task<IActionResult> Create(UserViewModel userViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                var myContent = JsonConvert.SerializeObject(userViewModel);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage createNewClaim = await client.PostAsync(client.BaseAddress + $"User", byteContent);
+
+                return RedirectToAction("Index");
+            }
+            return View(userViewModel);
+        }
+
+        [HttpGet("ExpenseClaim/Edit")]
+        public async Task<IActionResult> EditByAdmin()
+        {
+            HttpResponseMessage responseCreateClaim = await client.GetAsync(client.BaseAddress + $"ExpenseClaim");
+            return View();
+
+        }
+
+        [HttpPost("User/Edit")]
+        public async Task<IActionResult> EditByAdmin(UserViewModel userViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                var myContent = JsonConvert.SerializeObject(userViewModel);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await client.PutAsync(client.BaseAddress + $"User/{userViewModel.Id}", byteContent);
+                return RedirectToAction("Index");
+            }
+            return View(userViewModel);
         }
     }
 }
---
+++ DRS.ExpenseManagementSystem.UI/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DRS.ExpenseManagementSystem.UI.Models
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public string? EmployeeCode { get; set; }
+        public string? Password { get; set; }
+        public bool? IsAccountLocked { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
