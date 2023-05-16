c8848a422dfd02487cdf14e7bbfcbe1e61a27743
Anish Ani <anishani0078@gmail.com>
2023-05-16 16:56:50 +0530

    created service extension

---
+++ DRS.ExpenseManagementSystem.WebAPI/Extensions/ServiceExtension.cs
@@ -0,0 +1,34 @@
+﻿using DRS.ExpenseManagementSystem.Abstraction.Repository;
+using DRS.ExpenseManagementSystem.Repository.Repository;
+using DRS.ExpenseManagementSystem.WebAPI.Models;
+
+namespace DRS.ExpenseManagementSystem.WebAPI.Extensions
+{
+    public static class ServiceExtension
+    {
+    }
+
+    public static void RegisterRepositories(this IServiceCollection services)
+    {
+        services.AddTransient<ExpensesManagementSystem_UpdatedContext>();
+        services.AddTransient<IClaimStatusRepository, AnonymousDataRepository>();
+        services.AddTransient<IDepartmentRepository, AppConfigRepository>();
+        services.AddTransient<IBandwiseReadingRepository, BandwiseReadingRepository>();
+        services.AddTransient<IImageRepository, ImageRepository>();
+        services.AddTransient<ILocationRepository, LocationRepository>();
+        services.AddTransient<IPlantConfigurationRepository, PlantConfigurationRepository>();
+        services.AddTransient<IResultRepository, ResultRepository>();
+        services.AddTransient<IThermalBandsConfigRepository, ThermalBandsConfigRepository>();
+        services.AddTransient<IThermalZoneRepository, ThermalZoneRepository>();
+        services.AddTransient<IAssetRepository, AssetRepository>();
+        services.AddTransient<IWorkOrderRepository, WorkOrderRepository>();
+        services.AddTransient<IFeedRateRepository, FeedRateRepository>();
+        services.AddTransient<IUserRepository, UserRepository>();
+        services.AddTransient<IThermalImageTaskRepository, ThermalImageTaskRepository>();
+        services.AddTransient<IThermalImageRepository, ThermalImageRepository>();
+        services.AddTransient<ITraceImageRepository, TraceImageRepository>();
+        services.AddTransient<ITenantRepository, TenantRepository>();
+        services.AddTransient<IForecastMaintenanceTrendChartRepository, ForecastMaintenanceTrendChartRepository>();
+
+    }
+}
