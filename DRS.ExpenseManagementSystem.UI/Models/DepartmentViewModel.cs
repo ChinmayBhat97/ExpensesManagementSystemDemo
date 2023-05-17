b55791e911b432f941d62852829a7975cb20b42c
Anish Ani <anishani0078@gmail.com>
2023-05-17 13:59:49 +0530

    created the user controller for super admin

---
+++ DRS.ExpenseManagementSystem.UI/Models/DepartmentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DRS.ExpenseManagementSystem.UI.Models
+{
+    public class DepartmentViewModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ DRS.ExpenseManagementSystem.UI/Models/ProjectViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DRS.ExpenseManagementSystem.UI.Models
+{
+    public class ProjectViewModel
+    {
+        public int Id { get; set; }
+        public int? EmpId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string? Client { get; set; }
+    }
+}
