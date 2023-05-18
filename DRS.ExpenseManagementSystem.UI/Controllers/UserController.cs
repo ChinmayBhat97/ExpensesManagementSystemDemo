2d100e160c550e5e1f0530402f4cdc231eb5a8bb
Anish Ani <anishani0078@gmail.com>
2023-05-18 17:14:42 +0530

    completed views for rxpense claim, and user

--- DRS.ExpenseManagementSystem.UI/Controllers/UserController.cs
+++ DRS.ExpenseManagementSystem.UI/Controllers/UserController.cs
@@ -42,9 +42,7 @@ namespace DRS.ExpenseManagementSystem.UI.Controllers
             }
             else
             {
-                // Handle the case where the HTTP request fails
-                // Return an appropriate response or redirect
-                // For now, let's return a default view with no data
+                
                 return View();
             }
         }
@@ -98,10 +96,27 @@ namespace DRS.ExpenseManagementSystem.UI.Controllers
             return View(userViewModel);
         }
 
+        //public async Task<IActionResult> IndexProject()
+        //{
+        //    HttpResponseMessage responseUserList = await client.GetAsync(client.BaseAddress + $"Project");
+        //    return View();
+        //}
+
+        [HttpGet]
         public async Task<IActionResult> IndexProject()
         {
-            HttpResponseMessage responseUserList = await client.GetAsync(client.BaseAddress + $"Project");
-            return View();
+            HttpResponseMessage responseHomePage = await client.GetAsync(client.BaseAddress + "Project");
+            if (responseHomePage.IsSuccessStatusCode)
+            {
+                var responseContent = await responseHomePage.Content.ReadAsStringAsync();
+                var model = JsonConvert.DeserializeObject<List<ProjectViewModel>>(responseContent);
+                return View(model);
+            }
+            else
+            {
+
+                return View();
+            }
         }
 
         [HttpGet("User/CreateProject")]
