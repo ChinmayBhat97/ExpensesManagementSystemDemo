db7ffe24087cb99231b3727e47040c23d70bae9f
Anish Ani <anishani0078@gmail.com>
2023-05-30 13:28:47 +0530

    repo

--- DRS.ExpenseManagementSystem.Repository/Repository/ExpenseClaimRepository.cs
+++ DRS.ExpenseManagementSystem.Repository/Repository/ExpenseClaimRepository.cs
@@ -68,7 +68,7 @@ namespace DRS.ExpenseManagementSystem.Repository
 
         public async Task<List<ExpenseClaim>> GetAllDetails()
         {
-            return await _dbContext.ExpenseClaims.Include(a => a.Dept).Include(r => r.StatusNavigation).Include(b => b.Project).ToListAsync();
+            return await _dbContext.ExpenseClaims.Include(a => a.Dept).Include(r => r.StatusNavigation).Include(b => b.Project).Include(z => z.Emp).ToListAsync();
         }
     }
 }
